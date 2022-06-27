37972b905aa9156c0d1aefb89d062cc669b093d3
Asad Khurshid <asadkhurshid20012001@gmail.com>
2022-06-27 13:04:50 +0500

    takes input from user and prints the string filling the inputs in the pre stored string

---
+++ main.py
@@ -0,0 +1,7 @@
+adj = input ("Enter the adjactive.")
+verb = input ("Enter the verb.")
+verb = input ("Enter the second verb.")
+famousPerson = input ("Enter the name of a famous person.")
+
+madlib = f"computer programming is so {adj}! It makes me so excited all the time becuase I love to {verb}. Stay hyfrated and {verb} Like you are {famousPerson}."
+print (madlib)
